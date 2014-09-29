a9515f9058f0e9555e4fd6f789402426c8ff3260
Lucas Czech <luc@s-cze.ch>
2014-09-29 14:42:23 +0200

    added long loop over all models

--- src/main.cpp
+++ src/main.cpp
@@ -6,48 +6,28 @@
 #include <assert.h>
 
 #include "pll/pll.h"
+#include "main.h"
 
 using namespace std;
 
-int main (int argc, char* argv[])
+void analyzeModel(string alignmentFilename, string model)
 {
-    // check if PHYLIP alignment file is given
-    if (argc == 1) {
-        cout << "No PHYLIP input file given, aborting." << endl;
-        return 0;
-    }
+    //string model = "1,1,1,1,1,2";
+    cout << endl << "=============================" << endl;
+    cout << "      Model " << model << endl;
 
     // parse the alignment file
-    cout << "Using PHYLIP input file '" << argv[1] << "'." << endl;
-    string alignmentFilename = argv[1];
+
     pllAlignmentData* alignmentData = pllParseAlignmentFile (PLL_FORMAT_PHYLIP, alignmentFilename.c_str());
 
-    // create a partition file containing only one partition
-    // (PLL does not provide a default "use all" partition and
-    // cannot take the partition via string parameter)
-    string partitionFilename = alignmentFilename + ".partition";
-    ofstream partitionFile (partitionFilename.c_str());
-    if (!partitionFile.is_open()) {
-        cout << "Unable to write partition file '" << partitionFilename << "', aborting." << endl;
-        return 0;
-    }
-    cout << "Writing partition file '" << partitionFilename << "'." << endl;
-    partitionFile << "DNA, Partition = 1-" << alignmentData->sequenceLength << "\n";
-    partitionFile.close();
+
 
     // parse the partiotion file and connect it to the alignment
     pllQueue* parts = pllPartitionParse (partitionFilename.c_str());
     if (pllPartitionsValidate (parts, alignmentData) == PLL_FALSE) {
         cout << "Something is wrong with the partitions, aborting." << endl;
-        return 0;
+        exit 0;
     }
-    partitionList* partitions = pllPartitionsCommit (parts, alignmentData);
-    pllAlignmentRemoveDups (alignmentData, partitions);
-
-    // optimize everything
-    partitions->partitionData[0]->optimizeBaseFrequencies   = true;
-    partitions->partitionData[0]->optimizeAlphaParameter    = true;
-    partitions->partitionData[0]->optimizeSubstitutionRates = true;
 
     // set PLL instance attributes
     pllInstanceAttr attr;
@@ -57,28 +37,66 @@ int main (int argc, char* argv[])
     attr.useRecom         = PLL_FALSE;
     attr.randomNumberSeed = 0x12345;
 
-    // create the PLL instance
     pllInstance* inst;
-    inst = pllCreateInstance (&attr);
+    partitionList* partitions;
+
+    partitions = pllPartitionsCommit (parts, alignmentData);
+    pllAlignmentRemoveDups (alignmentData, partitions);
+
+    // optimize everything
+    partitions->partitionData[0]->optimizeBaseFrequencies   = true;
+    partitions->partitionData[0]->optimizeAlphaParameter    = true;
+    partitions->partitionData[0]->optimizeSubstitutionRates = true;
 
     // load the alignment
+    inst = pllCreateInstance (&attr);
     pllTreeInitTopologyForAlignment (inst, alignmentData);
     pllLoadAlignment (inst, alignmentData, partitions, PLL_DEEP_COPY);
 
     // create a tree
     pllComputeRandomizedStepwiseAdditionParsimonyTree (inst, partitions);
 
-    //double* empiricalFrequencies;
-    //empiricalFrequencies = (double*) malloc((size_t) 4 * sizeof(double));
-    //pllInitModel (inst, &empiricalFrequencies, partitions);
     pllInitModel (inst, partitions, alignmentData);
 
-    //pllSetSubstitutionRateMatrixSymmetries (char *string, partitionList * pr, int model);
+    pllSetSubstitutionRateMatrixSymmetries (const_cast<char*>(model.c_str()), partitions, 0);
     pllOptimizeModelParameters (inst, partitions, 0.1);
-    pllDestroyInstance (inst);
 
     // Destroy the PLL instance
     pllAlignmentDataDestroy (alignmentData);
     pllQueuePartitionsDestroy (&parts); // for some reason, PLL expects **pQueue here...
+    pllPartitionsDestroy (inst, &partitions); // again, expects a **partitionList here
+    pllDestroyInstance (inst);
+}
+
+int main (int argc, char* argv[])
+{
+    // check if PHYLIP alignment file is given
+    if (argc == 1) {
+        cout << "No PHYLIP input file given, aborting." << endl;
+        exit 0;
+    }
+    string alignmentFilename = argv[1];
+    cout << "Using PHYLIP input file '" << alignmentFilename << "'." << endl;
+
+    // create a partition file containing only one partition
+    // (PLL does not provide a default "use all" partition and
+    // cannot take the partition via string parameter)
+    string partitionFilename = alignmentFilename + ".partition";
+    ofstream partitionFile (partitionFilename.c_str());
+    if (!partitionFile.is_open()) {
+        cout << "Unable to write partition file '" << partitionFilename << "', aborting." << endl;
+        exit 0;
+    }
+    cout << "Writing partition file '" << partitionFilename << "'." << endl;
+    partitionFile << "DNA, Partition = 1-" << alignmentData->sequenceLength << "\n";
+    partitionFile.close();
+
+    // try out every substitution model to find the best one
+    // (PLL cannot reuse the data, so we have to do all the input file reading
+    // anew for every substitution model)
+    for (string model : MODEL_STRINGS) {
+        analyzeModel(alignmentFilename, model);
+    }
+
     return 0;
 }
